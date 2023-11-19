cc7a880831cdc8e95a9c5fcf867e0e257932a316
Chris De Asis <138704176+cde-as@users.noreply.github.com>
2023-11-19 09:03:09 -0800

    Created assertEqual function

--- head.js
+++ head.js
@@ -7,11 +7,14 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const head = function (arr) {
+  return arr[0];
+}
+
 
 // TEST CODE
-//assertEqual("Lighthouse Labs", "Bootcamp");
-//assertEqual(1, 1);
-// assertEqual("hi", "hi");
-// assertEqual(1, -1);
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), 0);
 
-module.exports = assertEqual;
+module.exports = head;
