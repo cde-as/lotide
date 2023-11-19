5efe76ed2e02a0b99019a6b7cf143c617d4609fe
Chris De Asis <138704176+cde-as@users.noreply.github.com>
2023-11-19 15:56:50 -0800

    Created eqArrays function

---
+++ eqArrays.js
@@ -0,0 +1,19 @@
+/* 
+Instructions:
+
+Implement a function eqArrays which takes in two arrays and returns true or 
+  false, based on a perfect match.
+ Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
+
+ */
+
+ const eqArrays = function(firstArray, secondArray) {
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+module.exports = eqArrays;
