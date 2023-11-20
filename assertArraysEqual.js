2cbc1eadfe6ac3bf167fdea624b1f1e75a1a2276
Chris De Asis <138704176+cde-as@users.noreply.github.com>
2023-11-19 21:01:21 -0800

    Added an initial conditional to check if array lengths are equal

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,6 +1,9 @@
 // Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
 
 const eqArrays = function(firstArray, secondArray) {
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  }
   for (let i = 0; i < firstArray.length; i++) {
     if (firstArray[i] !== secondArray[i]) {
       //console.log(false);
