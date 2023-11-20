3c47a5ac9a7d0fc73c04083fd7ba55e5eccb9657
Chris De Asis <138704176+cde-as@users.noreply.github.com>
2023-11-19 20:51:21 -0800

    Added in the first conditional

--- eqArrays.js
+++ eqArrays.js
@@ -8,6 +8,9 @@ Implement a function eqArrays which takes in two arrays and returns true or
  */
 
  const eqArrays = function(firstArray, secondArray) {
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  }
   for (let i = 0; i < firstArray.length; i++) {
     if (firstArray[i] !== secondArray[i]) {
       return false;
