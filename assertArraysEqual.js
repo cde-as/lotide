6fca0ed21b68fb168dc8089561c8db7f6ae377e4
Chris De Asis <138704176+cde-as@users.noreply.github.com>
2023-11-19 20:57:45 -0800

    Created assertArraysEqual func, and implementing prev eqArrays func

---
+++ assertArraysEqual.js
@@ -0,0 +1,25 @@
+// Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
+
+const eqArrays = function(firstArray, secondArray) {
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      //console.log(false);
+      return false;
+    }
+  }
+  //console.log(true);
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  const areEqual = eqArrays(actual, expected);
+
+  if (areEqual) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, "3"]); // Should return false
+assertArraysEqual([1, 2, 3], [1, 2, 4]); // Should return false 
