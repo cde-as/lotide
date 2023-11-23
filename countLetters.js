d9f93c8a0c11a004a119815987b9f6a0def55d80
Chris De Asis <138704176+cde-as@users.noreply.github.com>
2023-11-23 12:54:48 -0800

    assertEqual func now works with countLetters func used function(string)[s] to access key

--- countLetters.js
+++ countLetters.js
@@ -1,5 +1,4 @@
-const assertEqual = function(actual, expected) {
-
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -10,25 +9,31 @@ const assertEqual = function(actual, expected) {
 //Function should take in a string and return a count of each letter
 // Return should be an object
 // Tip: You can use for...of loop with strings
-const countLetters = function(string) {
+const countLetters = function (string) {
   const result = {};
 
-  for (const letter of string) { // for each letter in the string
-    console.log(string[letter]);
-    if (string[letter]) { //if the letter exists in the string
-    
-      if (result[letter]) { // and if the letter already exists in result object
-        result[letter] += 1;
+  for (let i = 0; i < string.length; i++) {
+    // for each letter in the string
+    //console.log(string[i]);
+    if (string[i]) {
+      //if the letter exists in the string
+      if (result[string[i]]) {
+        // and if the letter already exists in result object
+        result[string[i]] += 1;
       } else {
-        result[letter] = 1;
+        result[string[i]] = 1;
       }
     }
   }
-  console.log(result);
+  //console.log(result);
   return result;
 };
 
+// -------- TEST CODE ------
+console.log(countLetters("LHL"));
+console.log(countLetters("apple"));
 
-
-// TEST CODE
-countLetters("LHL");
+//console.log(countLetters("LHL")["L"]); // Returns: 2 -> This gives us access to the value of the key "L"
+assertEqual(countLetters("LHL")["L"], 2);
+assertEqual(countLetters("apple")["p"], 2);
+assertEqual(countLetters("apple")["a"], 2);
