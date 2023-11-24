2923f54f2ea771723c45d386c508421e7fdbd7b2
Chris De Asis <138704176+cde-as@users.noreply.github.com>
2023-11-23 18:02:11 -0800

    Main function works, not yet connected to assertArrayEquals function, a bit hard to read so I will optimize in the future

---
+++ letterPositions.js
@@ -0,0 +1,44 @@
+const eqArrays = function(firstArray, secondArray) {
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  }
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  const areEqual = eqArrays(actual, expected);
+
+  if (areEqual) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//Needs to skip spaces
+const letterPositions = function(sentence) {
+  const result = {};
+
+  for (let i = 0; i < sentence.length; i++) { //for every index in sentence
+    if (sentence[i] !== " ") {
+      if (result[sentence[i]]) {
+        result[sentence[i]].push(i);
+      } else {
+        result[sentence[i]] = [i];
+      }
+    }
+  }
+  console.log(result);
+  return result;
+};
+
+
+// --- TEST CODE ---
+//assertArraysEqual(letterPositions("hello").e, [1]);
+
+letterPositions("lighthouse in the house");
