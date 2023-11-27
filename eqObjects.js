87008a061fa2749ec957d22faefbe150585f86c6
Chris De Asis <138704176+cde-as@users.noreply.github.com>
2023-11-26 21:01:46 -0800

    created first conditional for eqObject function. Need to define for loop

---
+++ eqObjects.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function (object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+console.log(eqObjects(shirtObject , anotherShirtObject)); // => true
