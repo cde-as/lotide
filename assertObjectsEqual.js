2a1ae34099c0fc119c87404df190f1e388f15aa6
Chris De Asis <138704176+cde-as@users.noreply.github.com>
2023-12-06 21:51:53 -0800

    fixed bug in assertObjectsEqual function where inspect(actual) was being compared to itself

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,51 +1,72 @@
-const eqObjects = function(object1, object2) {
-  const keys1 = Object.keys(object1);
-  const keys2 = Object.keys(object2);
+const assertEqual = function(actual, expected) {
 
-  //if the length of keys are not the same it's not equal return false
-  if (keys1.length !== keys2.length) {
-    return false;
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
-  for (let key of keys1) {
-    console.log(object1[key]);
-    if (object1[key] !== object2[key]) {
+};
+
+const eqArrays = function(firstArray, secondArray) {
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
       return false;
     }
   }
   return true;
 };
 
-//  ------- FUNCTION IMPLEMENTATION ----------
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  const keys1 = Object.keys(object1); // gives us an array of keys from Object1
+  const keys2 = Object.keys(object2);
+
+  for (let key of keys1) {
+    if (Array.isArray(object2[key]) && Array.isArray(object1[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else {
+      if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
 
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require("util").inspect; // <= add this line
   console.log(`Example label: ${inspect(actual)}`);
+  console.log(`Example label: ${inspect(expected)}`);
 
   if (eqObjects(actual, expected)) {
     console.log(
-      `✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(actual)} `
+      `✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)} `
     );
   } else {
-    (actual !== expected);
-    console.log(`❌ Assertion Failed: ${inspect(actual)} !== ${inspect(actual)}`
+    console.log(`❌ Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`
     );
   }
 };
 
-// ---------- TEST CODE -------------
-//We need to use that return value in combination with assertEquals to test if the function is working correctly.
+
 const shirtObject = { color: "red", size: "medium" };
 const anotherShirtObject = { size: "medium", color: "red" };
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
 
-console.log(eqObjects(shirtObject, anotherShirtObject)); // => true
-assertObjectsEqual(eqObjects(shirtObject, anotherShirtObject), true);
+//We need to use that return value in combination with assertEquals to test if the function is working correctly.
 
-const longSleeveShirtObject = {
-  size: "medium",
-  color: "red",
-  sleeveLength: "long",
-};
-eqObjects(shirtObject, longSleeveShirtObject); // => false
+//console.log(eqObjects(shirtObject , anotherShirtObject)); // => true
+assertEqual(eqObjects(shirtObject , anotherShirtObject), true);
+// -----------------------
 
-assertObjectsEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
-assertObjectsEqual(eqObjects(shirtObject, longSleeveShirtObject), true); // => should return failed
+eqObjects(shirtObject , longSleeveShirtObject); // => false
+assertObjectsEqual(eqObjects(shirtObject , longSleeveShirtObject), false);
+
+// -----------------------
+assertObjectsEqual(shirtObject, longSleeveShirtObject);
