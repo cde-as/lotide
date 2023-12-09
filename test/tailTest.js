bd9c886b0dcbbaa31c37342be0a84c8fce701264
Chris De Asis <138704176+cde-as@users.noreply.github.com>
2023-12-08 20:42:05 -0800

    refactored with mocha/chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,26 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-assertEqual(words.length, 3);
-assertEqual(tail([5,6,7]).length, 2);
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), 3); // checking for failure
-assertEqual(tail([]), undefined);
+/*  Using assert.deepEqual to make sure contents are equal vs. strictEqual does not let us check for content equality but we can check for things like length as we're comparing number values*/
+
+describe("#tail", () => {
+  it("returns [6,7] for ([5,6,7])", () => {
+    assert.deepEqual(tail([5, 6, 7]), [6,7]);
+  });
+});
+
+it("returns 2 for ['Hello', 'Lighthouse', 'Labs'].length", () => {
+  assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"]).length, 2);
+});
+
+it("returns [ ] for an empty array", () => {
+  assert.strictEqual(tail([]).length, 0);
+});
+
+it("returns [ ] for an empty array", () => {
+  assert.deepEqual(tail([]), []);
+});
+
+it("returns [ ] for an array with only one variable", () => {
+  assert.deepEqual(tail([1]), []);
+});
